619b819f652a3f2e5564c08624732c489364bcc6
Thomas <Thomaschen14@gmail.com>
2021-01-07 16:27:26 -0800

    File

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(document).ready(function(){
+        $("button").click(function(){
+          $("h1").fadeOut(slow);
+/*           $("#div2").fadeOut("slow");
+          $("#div3").fadeOut(3000);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        })
+    })
+})
