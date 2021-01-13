7ecaf2bd9b166a90c485057ee57af70f9388367e
Thomas <Thomaschen14@gmail.com>
2021-01-12 16:25:21 -0800

    File

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $(document).ready(function(){
-        $("button").click(function(){
-          $("h1").fadeOut(slow);
-/*           $("#div2").fadeOut("slow");
-          $("#div3").fadeOut(3000);
- */
-
+        $("#button").click(async function(){
+            var response=await fetch(`https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand`)
+            $("#quote").slideUp();
+            var data=await response.json()
+            console.log(data)
+            $("#quote").html(data[0].content.rendered)
+            $("#quote").slideDown();
 
 
 
