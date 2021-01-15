39f45da9979df5e3d8b31ca82795f425101f1f11
Thomas <Thomaschen14@gmail.com>
2021-01-14 16:23:19 -0800

    File

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
         $("#button").click(async function(){
             var response=await fetch(`https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand`)
             $("#quote").slideUp();
+            $("#author").slideUp();
             var data=await response.json()
             console.log(data)
-            $("#quote").html(data[0].content.rendered)
+            var Random=Math.floor((Math.random() * 10));
+            console.log(Random)
+            $("#quote").html(data[Random].content.rendered)
+            $("#author").html("-"+data[Random].title.rendered)
             $("#quote").slideDown();
+            $("#author").slideDown();
 
 
 
